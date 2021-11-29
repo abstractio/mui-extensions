1780d7773dd713b9d72948187354fded73d68c5e
Aleksander Mirowsky <alex.xande10@gmail.com>
2021-11-29 04:53:37 -0300

    [feature] helper types to enable type inference throughout the stylesheet tree for easier overriding at parent components

--- packages/sx-stylesheet/src/lib/stylesheet.types.ts
+++ packages/sx-stylesheet/src/lib/stylesheet.types.ts
@@ -1,4 +1,5 @@
 import { SxProps, Theme } from '@mui/system';
+import { Stylesheet } from '..';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type StylesheetHandler = (...args: any[]) => SxProps<Theme>;
@@ -10,3 +11,19 @@ export type ThemeCSSProperties = {
 export type PartialRecord<K extends keyof never, T> = {
   [P in K]?: T;
 };
+
+type DeepPartial<T> = {
+  [P in keyof T]?: DeepPartial<T[P]>;
+};
+
+// I would rather not use this since it absolutely tanks vscode typescript's server perfomance
+export type StylesheetDeepTree<
+  T extends
+    | typeof Stylesheet['create']
+    | ReturnType<(...args: unknown[]) => typeof Stylesheet['create']>
+> = DeepPartial<Record<keyof DeepPartial<ReturnType<T>>, SxProps<Theme>>>;
+
+export type StylesheetShallowTree<T extends ThemeCSSProperties> = PartialRecord<
+  keyof T,
+  SxProps<Theme>
+>;
